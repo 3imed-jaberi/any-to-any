57beaccc9e5c02bad0dd465f36f11822fed8cb20
imed jaberi <imed_jebari@hotmail.fr>
2019-08-25 19:02:04 +0200

    update prod folder ---> update src ⚖️ ..

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,2 @@
-export declare function Convert(InputNumber: string | number, InputBase: number, OutputBase: number): string;
+declare function Convert(InputNumber: string | number, InputBase: number, OutputBase: number): string;
+export default Convert;
--- dist/src/convert-between-bases.d.ts
+++
@@ -1,7 +0,0 @@
-declare class ConvertBetweenBases {
-    private anyBaseToDecimal;
-    private decimalToAnyBase;
-    private validationsInput;
-    main(InputNumber: string | number, InputBase: number, OutputBase: number): string;
-}
-export default ConvertBetweenBases;
