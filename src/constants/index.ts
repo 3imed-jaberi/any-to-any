29e2b199177e3873276c89fc6d97b047c8fb934d
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-13 20:26:05 +0100

    add all errors in const dir ⛔️ ..

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+
+const __Error_InputBase__: string = 'Error InputBase : the input base should be between 2 et 36 ..';
+
+const __Error_OutputBase__ = 'Error OutputBase : the output base should be between 2 et 36 ..';
+
+const __Error_InputNumber_Size__ = 'Error InputNumber : the input number should be not empty string ..';
+
+const __Error_InputNumber_Int__ = 'Error InputNumber : the input number should be Integer or not possesses < \',\' or \'.\' >  .. ';
+
+const __Error_InputNumber_Not_Special_Charts__ = 'Error InputNumber : the input number should be not possesses special charts ..';
+
+const __Error_InputNumber_Less_Than_InputBase__ = 'Error InputNumber : each unit in the input number should be less than or equal the input base ..';
+
+
+export {
+  __Error_InputBase__,
+  __Error_OutputBase__,
+  __Error_InputNumber_Size__,
+  __Error_InputNumber_Int__,
+  __Error_InputNumber_Not_Special_Charts__,
+  __Error_InputNumber_Less_Than_InputBase__
+};
