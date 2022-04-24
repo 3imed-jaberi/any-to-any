63b01ecbd984014c0b9609467e88f618c0afff22
3imed-jaberi <imed_jebari@hotmail.fr>
2022-04-24 18:08:17 +0200

    update source code 🚀 ..

--- index.ts
+++ index.ts
@@ -5,23 +5,44 @@
  * MIT Licensed
  */
 
-import 'colors';
+import 'colors'
+
+/**
+ * TODO: remove experimentalNextConvert when we fix the problem when convert large hexa number to decimal.
+ *
+ * INFO:
+ *  convert('aa309be366a5cdd63103b657465f6f9617fbaee', 16, 10)
+ *  should equal '60725777912152566396085878260777866453342730990'
+ *  but return '60725777912152552488440220822060464062280464800'.
+ */
+import { experimentalConvert } from './experimental-convert.js'
+
+/**
+ * Utility to extract the unit to convert it.
+ *
+ * @api public
+ */
+function getUnitToConvertIt(number: string, fromDecimal?: boolean) {
+  return +number >= 0 && +number <= 9
+    ? +number
+    : !fromDecimal
+    ? number.charCodeAt(0) - 55
+    : String.fromCharCode(+number + 55)
+}
 
 /**
  * Convert from base 10 to any base.
- * 
+ *
  * @api public
  */
-function decimalToAnyBase (iNumber: number, oBase: number): string { 
-  let result = '', unitConverted, unitExtracted 
-  
-  while (iNumber !== 0) {
-    unitExtracted = iNumber % oBase
-    unitConverted = (unitExtracted >= 0 && unitExtracted <= 9) 
-      ? `${unitExtracted}`
-      : String.fromCharCode(unitExtracted + 55)
-    result = unitConverted + result
-    iNumber = parseInt(`${iNumber / oBase}`)
+function decimalToAnyBase(iNumber: number, oBase: number): string {
+  let result = ''
+
+  while (iNumber > 0) {
+    result =
+      String(getUnitToConvertIt(String((iNumber % oBase).toFixed()), true)) +
+      result
+    iNumber = Math.floor(iNumber / oBase)
   }
 
   return result
@@ -29,60 +50,60 @@ function decimalToAnyBase (iNumber: number, oBase: number): string {
 
 /**
  * Convert from any base to base 10.
- * 
+ *
  * @api public
  */
-function anyBaseToDecimal (iNumber: string, iBase: number): number {
+function anyBaseToDecimal(iNumber: string, iBase: number): number {
   // we don't need to make any convertion when we pass iBase with 10 value
   if (iBase === 10) return +iNumber
 
-  // otherwise, convert to base 10
-  let index = iNumber.length - 1, result = 0, power = 1, unitConverted
-
-  for (index; index >= 0; index--) {
-    unitConverted = (+iNumber[index] >= 0 &&  +iNumber[index] <= 9)
-      ? +iNumber[index]
-      : (iNumber[index].charCodeAt(0) - 55)      
-    result += unitConverted * power
-    power *= iBase
-  }
-
-  return result
+  return iNumber
+    .split('')
+    .reduce(
+      (prevResult, currentItem, index) =>
+        prevResult +
+        +getUnitToConvertIt(currentItem) *
+          Math.pow(iBase, iNumber.length - 1 - index),
+      0
+    )
 }
 
 /**
  * Utility to check passed number is on the range or not.
- * 
+ *
  * @api private
  */
-const isInRange = (iNumber: string, iBase: number) => ((+iNumber - 0) * (+iNumber - (iBase - 1)) <= 0)
+function isInRange(iNumber: string, iBase: number) {
+  return (+iNumber - 0) * (+iNumber - (iBase - 1)) <= 0
+}
 
 /**
  * Utility to invoke the convert process.
- * 
+ *
  * @api private
  */
 function convertEngine(iNumber: string, iBase: number, oBase: number): string {
-  // early return when we pass 0 (0 always equal 0) - with out sign -
-  if (iNumber === '0') return iNumber
   // https://github.com/SeanCannon/aybabtu#32-bit-limitation
-  if (iNumber.length > 32)
+  if (iNumber.length > 32 && iBase < 10)
     return (iNumber.match(/.{1,32}/g) as string[])
-      .map(chunk => convertEngine(chunk, iBase, oBase))
+      .map((chunk) => convertEngine(chunk, iBase, oBase))
       .join('')
-  return (
-    iBase === oBase
-      ? iNumber
-      : decimalToAnyBase(anyBaseToDecimal(iNumber, iBase), oBase)
-  )
+
+  return iBase === oBase
+    ? iNumber
+    : decimalToAnyBase(anyBaseToDecimal(iNumber, iBase), oBase)
 }
 
 /**
  * Convert from any base to any base.
- * 
+ *
  * @api public
  */
-function convert (iNumber: string|number, iBase: number, oBase: number): any {
+function convert(
+  iNumber: string | number,
+  iBase: number,
+  oBase: number
+): string {
   // force iNumber to be uppercase string
   iNumber = `${iNumber}`.toUpperCase()
 
@@ -98,8 +119,10 @@ function convert (iNumber: string|number, iBase: number, oBase: number): any {
 
   // validator.
   // check if exist some special charts.
-  if(iNumber.match(/[0-9A-Z]/g)?.length !== iNumber.length)
-    throw new Error('The input number should be not have special charts or empty.')
+  if (iNumber.match(/[0-9A-Z]/g)?.length !== iNumber.length)
+    throw new Error(
+      'The input number should be not have special charts or empty.'
+    )
 
   // check the input base.
   if (iBase < 2 || iBase > 36)
@@ -107,19 +130,29 @@ function convert (iNumber: string|number, iBase: number, oBase: number): any {
 
   // check the outbut base.
   if (oBase < 2 || oBase > 36)
-    throw new Error ('The output base should be between 2 et 36.')
+    throw new Error('The output base should be between 2 et 36.')
 
-  // TODO: work around this.
+  // TODO: work around this in the near future.
   if (iBase > 10) {
-    console.warn(`
+    console.warn(
+      `
       Please, be careful and ensure that you pass valid input number which respect the range of the passed bases.
       The module isn't checking the range for input bases greater than 10. soony, will do!
-    `.red)
+    `.red
+    )
   } else {
-    const normalizediNumber = iNumber.split('').filter((number) => isInRange(number, iBase)).join('')
+    const normalizediNumber = iNumber
+      .split('')
+      .filter((number) => isInRange(number, iBase))
+      .join('')
     if (iNumber !== normalizediNumber)
-      throw new Error (`The input number '${iNumber}' isn't on the range [0, ${iBase - 1}].`)
+      throw new Error(
+        `The input number '${iNumber}' isn't on the range [0, ${iBase - 1}].`
+      )
   }
+
+  // early return when we pass 0 (0 always equal 0) - with out sign -
+  if (iNumber === '0') return iNumber
 
   return sign + convertEngine(iNumber, iBase, oBase)
 }
@@ -127,5 +160,10 @@ function convert (iNumber: string|number, iBase: number, oBase: number): any {
 /**
  * Expose.
  */
-export { anyBaseToDecimal, decimalToAnyBase, convert, convert as default }
-// 26.372
+export {
+  anyBaseToDecimal,
+  decimalToAnyBase,
+  convert,
+  experimentalConvert,
+  convert as default,
+}
